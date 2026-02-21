9f87e50afdcd75250644a158588f86a85ec76255
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-21 12:58:40 +0500

    Update Navbar component colors for improved visual consistency by changing the title and button background colors to a unified shade.

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
         <nav className='bg-[#000310] w-full h-20 flex items-center justify-between px-30 py-4'>
             <div className='flex justify-center items-center gap-2'>
                 <Image src="/logo.png" alt="logo" width={40} height={40} className='rounded'/>
-                <h1 className='text-2xl text-[#5A5DC1]'>TASKIFY</h1>
+                <h1 className='text-2xl text-[#656FE4]'>TASKIFY</h1>
             </div>
             <ul className='w-full flex justify-center items-center gap-8'>
                 {nav_items.map((item, index) => (
@@ -16,7 +16,7 @@ function Navbar() {
                     </li>
                 ))}
             </ul>
-            <button className='bg-[#5A5DC1] px-6 py-2.5 rounded-4xl text-md'>Signup</button>
+            <button className='bg-[#656FE4] px-6 py-2.5 rounded-4xl text-md'>Signup</button>
         </nav>
     )
 }
