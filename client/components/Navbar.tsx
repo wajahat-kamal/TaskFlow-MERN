00b21b297da558870666acb5cbbf762dba165e71
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-21 12:47:00 +0500

    Refactor Navbar component to improve styling and layout. Update button styles for better visibility and adjust spacing in the navigation items for enhanced user experience. Remove commented-out logo image for cleaner code.

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -5,17 +5,17 @@ function Navbar() {
     return (
         <nav className='bg-[#000310] w-full h-20 flex items-center justify-between px-20 py-4'>
             <div>
-                <img src="" alt="logo" />
+                {/* <img src="" alt="logo" /> */}
                 <h1>Taskify</h1>
             </div>
-            <ul className='w-full flex justify-center items-center gap-3'>
+            <ul className='w-full flex justify-center items-center gap-6'>
                 {nav_items.map((item, index) => (
                     <li key={item + index} >
                         {item}
                     </li>
                 ))}
             </ul>
-                <button>Sign up</button>
+            <button className='bg-[#8A77F2] px-6 py-2.5 rounded-4xl text-md'>Signup</button>
         </nav>
     )
 }
