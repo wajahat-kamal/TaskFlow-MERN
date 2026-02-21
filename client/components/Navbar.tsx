47303d759f9d18816a623dd6d16b79e02c3617d2
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-21 13:16:06 +0500

    Refactor Navbar component to improve navigation structure and responsiveness. Updated navigation items to include links, enhanced mobile menu functionality, and adjusted layout for better user experience.

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,9 +1,15 @@
+"use client"
 import { Menu, X } from 'lucide-react';
 import Image from 'next/image'
 import React, { useState } from 'react'
 
 function Navbar() {
-    const nav_items = ["Home", "About", "Tasks", "Pricing", "Products"];
+    const nav_items = [
+        { name: "Home", href: "/" },
+        { name: "About", href: "#about" },
+        { name: "Pricing", href: "#menu" },
+        { name: "Products", href: "#reviews" },
+    ]
 
     const [isOpen, setIsOpen] = useState(false)
     return (
@@ -12,19 +18,50 @@ function Navbar() {
                 <Image src="/logo.png" alt="logo" width={40} height={40} className='rounded' />
                 <h1 className='text-2xl text-[#656FE4]'>TASKIFY</h1>
             </a>
-            <div className='flex justify-between items-center md:w-[100%] w-40'>
+            <div className='flex justify-between items-center md:w-[100%] w-34'>
                 <ul className='hidden md:flex w-full justify-center items-center gap-8'>
                     {nav_items.map((item, index) => (
-                        <li className='hover:text-[#656FE4] cursor-pointer' key={item + index} >
-                            {item}
+                        <li key={item.name + index}>
+                            <a
+                                href={item.href}
+                                onClick={() => setIsOpen(false)}
+                                className="block text-white text-lg font-medium hover:text-yellow-400 transition"
+                            >
+                                {item.name}
+                            </a>
                         </li>
                     ))}
                 </ul>
                 <button className='bg-[#656FE4] px-6 py-2.5 rounded-4xl text-md'>Signup</button>
-                <div className='md:hidden block'>
-                    {isOpen ? <X/> : <Menu/>}
-                </div>
-                {isOpen && <aside className='md:hidden block'>hey</aside>}
+                <div>{isOpen ? (
+                    <button onClick={() => setIsOpen(false)}><X/></button>
+                ) : (
+                    <button onClick={() => setIsOpen(true)}><Menu/></button>
+                )}</div>
+
+                <aside
+                    className={`fixed top-0 right-0 h-full w-72 bg-[#0B111E] backdrop-blur-md shadow-2xl transform transition-transform duration-300 ease-in-out z-60 ${isOpen ? "translate-x-0" : "translate-x-full"
+                        }`}
+                >
+                    {/* Links */}
+                    <nav className="mt-10 flex-1">
+                        <ul className="flex flex-col space-y-6">
+                            {nav_items.map((item, index) => (
+                                <li key={item.name + index}>
+                                    <a
+                                        href={item.href}
+                                        onClick={() => setIsOpen(false)}
+                                        className="block text-white text-lg font-medium hover:text-yellow-400 transition"
+                                    >
+                                        {item.name}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+
+
+                </aside>
             </div>
         </nav>
     )
