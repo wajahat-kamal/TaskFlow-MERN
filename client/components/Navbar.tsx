567f0e09e9ae317e970038ab31fef3f0074eeda1
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-21 13:05:37 +0500

    Update package dependencies and Navbar component layout. Added axios and motion libraries for enhanced functionality, and refined Navbar styling for improved responsiveness and visual appeal.

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -4,19 +4,23 @@ import React from 'react'
 function Navbar() {
     const nav_items = ["Home", "About", "Tasks", "Pricing", "Products"]
     return (
-        <nav className='bg-[#000310] w-full h-20 flex items-center justify-between px-30 py-4'>
-            <div className='flex justify-center items-center gap-2'>
-                <Image src="/logo.png" alt="logo" width={40} height={40} className='rounded'/>
+        <nav className='bg-[#000310] w-full h-20 flex items-center justify-between px-6 md:px-30 py-4'>
+            <a href='#' className='flex justify-center items-center gap-2'>
+                <Image src="/logo.png" alt="logo" width={40} height={40} className='rounded' />
                 <h1 className='text-2xl text-[#656FE4]'>TASKIFY</h1>
+            </a>
+            <div className='flex justify-between items-center md:w-[100%] w-40'>
+                <ul className='hidden md:flex w-full justify-center items-center gap-8'>
+                    {nav_items.map((item, index) => (
+                        <li className='hover:text-[#656FE4] cursor-pointer' key={item + index} >
+                            {item}
+                        </li>
+                    ))}
+                </ul>
+                <button className='bg-[#656FE4] px-6 py-2.5 rounded-4xl text-md'>Signup</button>
+                <aside className='md:hidden block'>hey</aside>
+
             </div>
-            <ul className='w-full flex justify-center items-center gap-8'>
-                {nav_items.map((item, index) => (
-                    <li key={item + index} >
-                        {item}
-                    </li>
-                ))}
-            </ul>
-            <button className='bg-[#656FE4] px-6 py-2.5 rounded-4xl text-md'>Signup</button>
         </nav>
     )
 }
